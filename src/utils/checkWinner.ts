14f41a147a743164bbbdaacc9af764aac41735dd
Filipe Dos Santos <filipesantos.psic@gmail.com>
2024-11-13 08:47:21 -0300

    [WIP]: 2

---
+++ src/utils/checkWinner.ts
@@ -0,0 +1,21 @@
+export const checkWinner = (squares: string[] | null) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  if (!squares) return null;
+
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+};

af95931be017cef30660ea1574c36ba0ca6e029a
Filipe Dos Santos <filipesantos.psic@gmail.com>
2024-11-15 20:45:35 -0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,12 @@ interface BoardProps {
   onPlay: (nextSquare: string[]) => void;
 }
 
-export function Board({ xIsNext, squares, onPlay}: BoardProps) {
-  
+export function Board({ xIsNext, squares, onPlay }: BoardProps) {
   const handleClick = (i: number) => {
     if (squares[i] || checkWinner(squares)) return;
 
     const nextSquares = squares.slice();
-    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O"); 
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     onPlay(nextSquares);
   };
 
@@ -49,34 +48,33 @@ export function Board({ xIsNext, squares, onPlay}: BoardProps) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquare: string[]) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquare];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
-  }
+  };
 
   const jumpTo = (nextMove: number) => {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
-  }
+  };
 
-  const moves = history.map((squares, move) => {
+  const moves = history.map((_, move) => {
     let description;
-    (move > 0 ? description = `Go to move # ${move}` : description = "Go to game start")
+    move > 0
+      ? (description = `Go to move # ${move}`)
+      : (description = "Go to game start");
 
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
-    )
-  } )
-  
+    );
+  });
 
   return (
     <div className="game">
